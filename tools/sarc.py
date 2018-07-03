ff67f7c9ebd808b874cbd1930c3dd5ec276814c8
Léo Lam <leo@innovatetechnologi.es>
2018-07-03 18:35:10 +0200

    tools: Add experimental support for making SARCs

--- tools/sarc.py
+++ tools/sarc.py
@@ -98,6 +98,100 @@ class SARC:
         end = self._data.find(_NUL_CHAR, offset)
         return self._data[offset:end].decode('utf-8')
 
+class _PlaceholderOffsetWriter:
+    """Writes a placeholder offset value that will be filled later."""
+    def __init__(self, stream: typing.BinaryIO, parent) -> None:
+        self._stream = stream
+        self._offset = stream.tell()
+        self._parent = parent
+    def write_placeholder(self) -> None:
+        self._stream.write(self._parent._u32(0xffffffff))
+    def write_offset(self, offset: int, base: int = 0) -> None:
+        current_offset = self._stream.tell()
+        self._stream.seek(self._offset)
+        self._stream.write(self._parent._u32(offset - base))
+        self._stream.seek(current_offset)
+    def write_current_offset(self, base: int = 0) -> None:
+        self.write_offset(self._stream.tell(), base)
+
+def _align_up(n: int) -> int:
+    return (n + 3) & -4
+
+class SARCWriter:
+    class File(typing.NamedTuple):
+        name: str
+        data: bytes
+
+    def __init__(self, be: bool) -> None:
+        self._be = be
+        self._hash_multiplier = 0x65
+        self._files: typing.Dict[int, SARCWriter.File] = dict()
+
+    def _hash_file_name(self, name: str) -> int:
+        h = 0
+        for c in name:
+            h = (ord(c) + h * self._hash_multiplier) & 0xffffffff
+        return h
+
+    def add_file(self, name: str, data: bytes) -> None:
+        self._files[self._hash_file_name(name)] = SARCWriter.File(name, data)
+
+    def write(self, stream: typing.BinaryIO) -> None:
+        # SARC header
+        stream.write(b'SARC')
+        stream.write(self._u16(0x14))
+        stream.write(self._u16(0xfeff))
+        file_size_writer = self._write_placeholder_offset(stream)
+        data_offset_writer = self._write_placeholder_offset(stream)
+        stream.write(self._u16(0x100))
+        stream.write(self._u16(0)) # Unused.
+
+        # SFAT header
+        stream.write(b'SFAT')
+        stream.write(self._u16(0xc))
+        stream.write(self._u16(len(self._files)))
+        stream.write(self._u32(self._hash_multiplier))
+
+        # Node information
+        sorted_hashes = sorted(self._files.keys())
+        string_offset = 0
+        data_offset = 0
+        for h in sorted_hashes:
+            stream.write(self._u32(h))
+            stream.write(self._u32(0x01000000 | (string_offset >> 2)))
+            stream.write(self._u32(data_offset))
+            data_offset += _align_up(len(self._files[h].data))
+            stream.write(self._u32(data_offset))
+            string_offset += _align_up(len(self._files[h].name) + 1)
+
+        # File name table
+        stream.write(b'SFNT')
+        stream.write(self._u16(8))
+        stream.write(self._u16(0))
+        for h in sorted_hashes:
+            stream.write(self._files[h].name.encode())
+            stream.write(_NUL_CHAR)
+            stream.seek(_align_up(stream.tell()))
+
+        # File data
+        data_offset_writer.write_current_offset()
+        for h in sorted_hashes:
+            stream.write(self._files[h].data)
+            stream.seek(_align_up(stream.tell()))
+
+        # Write the final file size.
+        file_size_writer.write_current_offset()
+
+    def _write_placeholder_offset(self, stream) -> _PlaceholderOffsetWriter:
+        p = _PlaceholderOffsetWriter(stream, self)
+        p.write_placeholder()
+        return p
+
+    def _u16(self, value: int) -> bytes:
+        return struct.pack(_get_unpack_endian_character(self._be) + 'H', value)
+    def _u32(self, value: int) -> bytes:
+        return struct.pack(_get_unpack_endian_character(self._be) + 'I', value)
+
 def read_file_and_make_sarc(f: typing.BinaryIO) -> typing.Optional[SARC]:
     f.seek(0)
     magic: bytes = f.read(4)
