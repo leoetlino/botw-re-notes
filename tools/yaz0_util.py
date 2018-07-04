1f47a45a431bb92bf8ec43eecfec1f34b4b98964
Léo Lam <leo@innovatetechnologi.es>
2018-07-04 09:37:10 +0200

    tools: yaz0_util: Fix type hint for functions

--- tools/yaz0_util.py
+++ tools/yaz0_util.py
@@ -1,12 +1,13 @@
 import os
 import subprocess
+from typing import Union
 
 _tool_name = 'wszst' if os.name != 'nt' else 'wszst.exe'
 
-def compress(data: bytes) -> bytes:
-    return subprocess.run([_tool_name, "comp", "-", "-d-", "-C10"], input=data, stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE, check=True).stdout
+def compress(data: Union[bytes, memoryview]) -> bytes:
+    return subprocess.run([_tool_name, "comp", "-", "-d-", "-C10"], input=data, # type: ignore
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True).stdout
 
-def decompress(data: bytes) -> bytes:
-    return subprocess.run([_tool_name, "de", "-", "-d-"], input=data, stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE, check=True).stdout
+def decompress(data: Union[bytes, memoryview]) -> bytes:
+    return subprocess.run([_tool_name, "de", "-", "-d-"], input=data, # type: ignore
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True).stdout
