73b4d57fdf3173f0604eb0a71fab5116e642f2c1
Léo Lam <leo@innovatetechnologi.es>
2019-01-11 22:06:49 +0100

    Add tool to show skipped actors during Ganon battle

--- tools/show_placement_gen_group.py
+++ tools/show_placement_gen_group.py
@@ -1,59 +1,11 @@
 import pprint
-import typing
 import yaml
 
 import argparse
 import byml
 import byml.yaml_util
 import wszst_yaz0
-
-class Link(typing.NamedTuple):
-    other_obj: dict
-    link_iter: dict
-    ltype: str
-    def description(self) -> str:
-        s = f"[0x{self.other_obj['HashId']:08x}] {self.other_obj['UnitConfigName']} - {self.ltype}"
-        params = self.link_iter.get('!Parameters')
-        if params:
-            s += f" {params}"
-        return s
-
-class Map:
-    def __init__(self, map_data) -> None:
-        self.map_data = map_data
-        self.objs: typing.Dict[int, dict] = dict()
-        for obj in map_data['Objs']:
-            self.objs[obj['HashId']] = obj
-            obj['__links'] = []
-            obj['__links_to_self'] = []
-
-    def get_obj(self, hashid) -> dict:
-        return self.objs[hashid]
-
-    def parse_obj_links(self) -> None:
-        for obj in self.map_data['Objs']:
-            links = obj.get('LinksToObj', None)
-            if not links:
-                continue
-            for link in links:
-                dest_obj = self.objs[link['DestUnitHashId']]
-                obj['__links'].append(Link(other_obj=dest_obj, link_iter=link, ltype=link['DefinitionName']))
-                dest_obj['__links_to_self'].append(Link(other_obj=obj, link_iter=link, ltype=link['DefinitionName']))
-
-    def build_gen_group(self, obj) -> typing.List[dict]:
-        gen_group: typing.Set[int] = set()
-        self._do_build_gen_group(obj, gen_group)
-        return [self.objs[hashid] for hashid in gen_group]
-
-    def _do_build_gen_group(self, obj, gen_group) -> None:
-        hashid = obj['HashId']
-        if hashid in gen_group:
-            return
-        gen_group.add(hashid)
-        for link in obj['__links']:
-            self._do_build_gen_group(link.other_obj, gen_group)
-        for link in obj['__links_to_self']:
-            self._do_build_gen_group(link.other_obj, gen_group)
+from _map_utils import Map
 
 def main() -> None:
     parser = argparse.ArgumentParser('Shows the placement generation group for a map object.')
---
+++ tools/show_skipped_actors_for_ganon.py
@@ -0,0 +1,91 @@
+import pprint
+import typing
+import yaml
+
+import argparse
+import byml
+import byml.yaml_util
+from pathlib import Path
+import wszst_yaz0
+import zlib
+from _map_utils import Map
+
+actorinfodata = byml.Byml((Path(__file__).parent.parent/'game_files'/'ActorInfo.product.byml').open('rb').read()).parse()
+def get_actor_data(name):
+    h = zlib.crc32(name.encode())
+    hashes = actorinfodata['Hashes']
+    a = 0
+    b = len(hashes) - 1
+    while a <= b:
+        m = (a + b) // 2
+        if hashes[m] < h:
+            a = m + 1
+        elif hashes[m] > h:
+            b = m - 1
+        else:
+            return actorinfodata['Actors'][m]
+    return None
+
+def is_flag4_actor(name):
+    if name == 'Enemy_GanonBeast':
+        return False
+    info = get_actor_data(name)
+    for x in ('Enemy', 'GelEnemy', 'SandWorm', 'Prey', 'Dragon', 'Guardian'):
+        if info['profile'] == x:
+            return True
+    if 'NPC' in info['profile']:
+        return True
+    return False
+
+def should_spawn_obj(obj):
+    name = obj['UnitConfigName']
+    if is_flag4_actor(name):
+        return False
+    if name == 'Enemy_Guardian_A':
+        return False
+    if 'Entrance' in name or 'WarpPoint' in name or 'Terminal' in name:
+        return False
+    return True
+
+def main() -> None:
+    parser = argparse.ArgumentParser('Shows actors that are not spawned when in final boss mode.')
+    parser.add_argument('map_path', help='Path to a map unit (BYAML or compressed BYAML or YAML)')
+    args = parser.parse_args()
+    MAP_PATH: str = args.map_path
+
+    byml.yaml_util.add_constructors(yaml.CSafeLoader)
+    with open(MAP_PATH, 'rb') as f:
+        if MAP_PATH.endswith('mubin'):
+            map_data = byml.Byml(wszst_yaz0.decompress(f.read())).parse()
+        else:
+            map_data = yaml.load(f, Loader=yaml.CSafeLoader)
+
+    pmap = Map(map_data)
+    pmap.parse_obj_links()
+
+    skip_reasons: typing.Dict[int, str] = dict()
+    skipped_objs: typing.Set[int] = set()
+    for obj in pmap.objs.values():
+        objid = obj['HashId']
+        if objid in skipped_objs or should_spawn_obj(obj):
+            continue
+        skipped_objs.add(objid)
+        gen_group = pmap.build_gen_group(obj)
+        for linked_obj in gen_group:
+            skipped_objs.add(linked_obj['HashId'])
+            skip_reasons[linked_obj['HashId']] = f'linked to skipped object: 0x{obj["HashId"]:08x} {obj["UnitConfigName"]}'
+        skip_reasons[objid] = 'skipped'
+
+    for objid in skipped_objs:
+        obj = pmap.get_obj(objid)
+        print(f"[0x{obj['HashId']:08x}] {obj['UnitConfigName']} {tuple(obj['Translate'])}")
+        if '!Parameters' in obj:
+            pprint.pprint(obj['!Parameters'], indent=2)
+        print(f"| SKIP REASON: {skip_reasons[objid]}")
+        for link in obj['__links']:
+            print(f"| LINKS TO: {link.description()}")
+        for link in obj['__links_to_self']:
+            print(f"| LINKED BY: {link.description()}")
+        print('-'*70)
+
+main()
